5e731b83f7a9c86f6f02033b0ee9bc01e897f74b
최사라 <fktkchl@naver.com>
2023-07-11 20:49:13 +0900

    Feat: 데이터를 가져와서 캐릭터 목록 생성하는 기능 추가 items.json 파일을 사용하여 캐릭터 데이터를 로드 JSON 파일에서 데이터를 가져오는 getData 함수를 구현 기존의 하드 코딩된 방식 대신, 데이터를 불러와 동적으로 캐릭터 목록을 생성하는 characterCollection 함수 구현

---
+++ js/characterGenerator.js
@@ -0,0 +1,37 @@
+const itemList = document.querySelector('.character-list');
+
+// 데이터 불러오기
+const getData = async () => {
+  try {
+    const response = await fetch('./items.json');
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// 상품 아이템 생성
+const characterCollection = (data) => {
+  let itemsEl = '';
+  data.forEach((el) => {
+    console.log(el.name);
+    itemsEl += `<li>
+    <button class="btn-character" type="button">
+      <img class="character-img" src="../img/${el.img}" alt="">
+      <span class="character-name">${el.name}</span>
+      <strong class="character-price">${el.price}</strong>
+    </button>
+  </li>`;
+  });
+  itemList.innerHTML = itemsEl;
+};
+
+const setup = async () => {
+  const data = await getData();
+  characterCollection(data);
+};
+export default setup;
---
+++ js/index.js
@@ -0,0 +1,3 @@
+import setup from './characterGenerator.js';
+
+setup();
