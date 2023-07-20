ddc510a48315a1e313b5360a42f40096f82d711f
최사라 <fktkchl@naver.com>
2023-07-20 21:04:27 +0900

    Feat 입금 및 거스름돈 반환 기능 추가 입금하면 잔액이 증가하고, 그 금액만큼 캐릭터 선택할 수 있는 기능 추가 거스름돈 반환할 경우 소지금으로 나머지 잔액 합해지는 기능 추가 소지금보다 입금액이 많을 경우 소지금이 부족하다는 알림 창 띄움 입금액에 숫자가 아닌 값이 입력된 경우 알림 창 띄움 잔액보다 선택한 캐릭터들의 총 금액이 더 클 경우 알림 창 띄움

--- js/vendingMachineEvents.js
+++ js/vendingMachineEvents.js
@@ -2,6 +2,12 @@ const cart = document.querySelector('.section1 .shopping-list');
 const obtainBtn = document.querySelector('.btn-obtain');
 const obtainedList = document.querySelector('.obtained-list');
 const total = document.querySelector('.total');
+const depositBtn = document.querySelector('.inputBtn');
+const inputDeposit = document.querySelector('#input-money');
+
+const balance = document.querySelector('.balance-money');
+const myMoney = document.querySelector('.money-in-hand');
+const changeBtn = document.querySelector('.changeBtn');
 let items;
 
 const initializeItems = () => {
@@ -46,6 +52,16 @@ const handleItem = (e) => {
   const target = e.currentTarget;
   const data = target.dataset;
   const cartItems = cart.children;
+
+  const cartTotal = getCartTotal();
+  const total = cartTotal + parseInt(e.currentTarget.dataset.price);
+  const balanceVal = parseInt(balance.textContent.replace(/[^0-9]/g, ''));
+
+  // 잔액 부족 예외 처리
+  if (total > balanceVal) {
+    alert('잔액이 부족합니다.');
+    return;
+  }
   let targetCartEl;
 
   for (const item of cartItems) {
@@ -97,6 +113,10 @@ const handleItemsObtain = () => {
   const totalVal = formatNum(currTotal + cartTotal);
   total.textContent = `총금액: ${totalVal}원`;
 
+  // 잔액 변경
+  const currBalanceVal = parseInt(balance.textContent.replace(/[^0-9]/g, ''));
+  const balanceVal = formatNum(currBalanceVal - cartTotal);
+  balance.textContent = `${balanceVal}원`;
   [...cartItems].forEach((item) => {
     // 획득한 캐릭터에 있다면 상품 수량 증가
     if (obtainedItemsName.includes(item.dataset.name)) {
@@ -116,6 +136,40 @@ const handleItemsObtain = () => {
   cart.innerHTML = '';
 };
 
+const handleDeposit = () => {
+  const depositVal = parseInt(inputDeposit.value);
+  const reg = new RegExp(/[^0-9]/, 'g');
+  if (reg.test(depositVal)) {
+    alert('숫자만 입력 가능합니다.');
+    inputDeposit.value = '';
+    return;
+  }
+  const currMyMoneyVal = parseInt(myMoney.textContent.replace(/[^0-9]/g, ''));
+  if (currMyMoneyVal < depositVal) {
+    alert('소지금이 부족합니다.');
+    return;
+  }
+
+  // 잔액 업데이트
+  const currBalanceVal = parseInt(balance.textContent.replace(/[^0-9]/g, ''));
+  const balanceVal = formatNum(currBalanceVal + depositVal);
+  balance.textContent = `${balanceVal}원`;
+
+  // 입금액 초기화
+  inputDeposit.value = '';
+
+  // 소지금 업데이트
+  const myMoneyVal = formatNum(currMyMoneyVal - depositVal);
+  myMoney.textContent = `${myMoneyVal}원`;
+};
+
+const handleChangeObtain = () => {
+  const currMyMoneyVal = parseInt(myMoney.textContent.replace(/[^0-9]/g, ''));
+  const currBalanceVal = parseInt(balance.textContent.replace(/[^0-9]/g, ''));
+  myMoney.textContent = `${formatNum(currMyMoneyVal + currBalanceVal)}원`;
+  balance.textContent = '0원';
+};
+
 const bundleOfEvents = () => {
   initializeItems();
 
@@ -123,6 +177,8 @@ const bundleOfEvents = () => {
     item.addEventListener('click', handleItem);
   });
   obtainBtn.addEventListener('click', handleItemsObtain);
+  depositBtn.addEventListener('click', handleDeposit);
+  changeBtn.addEventListener('click', handleChangeObtain);
 };
 
 export default bundleOfEvents;
