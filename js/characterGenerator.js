d2f0f6eeac7019113be19e4c958bd95a8fa04d3c
최사라 <fktkchl@naver.com>
2023-07-20 21:32:31 +0900

    Feat: 가격을 숫자에서 문자열로 변환하여 쉼표 추가 캐릭터 가격을 숫자에서 문자열로 변환하여 쉼표 추가

--- js/characterGenerator.js
+++ js/characterGenerator.js
@@ -14,18 +14,26 @@ const getData = async () => {
   }
 };
 
+// 숫자 형식 변경
+const formatNum = (num) => {
+  return new Intl.NumberFormat().format(num);
+};
+
 // 상품 아이템 생성
 const characterCollection = (data) => {
   let itemsEl = '';
   data.forEach((el) => {
+    // 가격을 숫자에서 문자열로 변환하여 쉼표 추가
+    const formattedPrice = formatNum(el.price);
     itemsEl += `<li>
     <button class="btn-character" type="button" data-name="${el.name}" data-price="${el.price}" data-img="${el.img}" data-count="${el.count}">
       <img class="character-img" src="../img/${el.img}" alt="">
       <span class="character-name">${el.name}</span>
-      <strong class="character-price">${el.price}</strong>
+      <strong class="character-price">${formattedPrice}원</strong>
     </button>
   </li>`;
   });
+
   itemList.innerHTML = itemsEl;
 };
 
